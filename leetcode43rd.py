130951097068653d43e1fe194ffd3768bf67547f
balasrinivasanammu <balajiit88@gmail.com>
2025-01-06 14:45:42 +0530

    Add files via upload

---
+++ leetcode43rd.py
@@ -0,0 +1,14 @@
+num1 = "123"
+num2 = "456"
+
+# Convert the strings to integers
+int_num1 = int(num1)
+int_num2 = int(num2)
+
+# Multiply the numbers
+result = int_num1 * int_num2
+
+# Convert the result back to a string
+result_str = str(result)
+
+print(result_str)
